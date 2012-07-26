850a1577aa9b3c04d909de5ea8c51bfabd0c1514
Eka Putra <ekaputra@balitechy.com>
2012-07-26 19:06:52 +0800

    Edit utils

--- menuz/utils.py
+++ menuz/utils.py
@@ -1,6 +1,17 @@
+from django.core.urlresolvers import reverse
 from menuz.models import Menuz, MenuzItem
 from menuz.registry import menuz
 from menuz.registry import get_menuz_object_model
+
+
+# used in conjunction with egocms app
+# part of Egomedia Bali's inhouse CMS project
+egosetting_available = True
+try:
+    from egosettings.egosetting import get_setting
+except ImportError:
+    egosetting_available = False
+    
 
 def get_menu_by_position(position):
     """
@@ -23,11 +34,23 @@ def get_menu_by_position(position):
                     model = get_menuz_object_model(obj.content_type)
                     try:
                         item = model.objects.get(pk=obj.content_id)
-                        all_items.append({'url': item.get_absolute_url(), 'title': obj.title})
-                    except:
-                        pass
-    except:
-        pass
+                        
+                        # works with egocms
+                        # this will check if current page setted as homepage_id
+                        # if yes, then display site base url
+                        if egosetting_available:
+                            homepage_id = get_setting('site_homepage')
+                            if homepage_id == item.id:
+                                all_items.append({'url': reverse('index'), 'title': obj.title})
+                            else:
+                                all_items.append({'url': item.get_absolute_url(), 'title': obj.title})
+                        else:
+                            all_items.append({'url': item.get_absolute_url(), 'title': obj.title})
+                            
+                    except Exception, err:
+                        print err
+    except Exception, err:
+        print err
     #return a tuple of title and its items
     return (menu_title, all_items)
 
